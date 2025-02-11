2e51d18b0ee7d4cde3336824498fcc6b96f94ecc
joao.ottoni <jvoo2016@gmail.com>
2025-02-11 19:13:16 -0300

    Fluxo main implementado

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,2 +1,74 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numeroConta;
+        String agencia, nomeCliente;
+        double saldo;
+
+        numeroConta = lerInteiro(scanner);
+        scanner.nextLine();
+
+        agencia = lerString(scanner, "Digite o número da agencia: ");
+
+        nomeCliente = lerString(scanner, "Digite o seu nome: ");
+
+        saldo = lerDouble(scanner);
+
+        Conta conta = new Conta(numeroConta, agencia, nomeCliente, saldo);
+
+        System.out.println(conta.toString());
+    }
+
+    private static int lerInteiro(Scanner scanner) {
+        int valorRetorno;
+        do {
+            System.out.print("Digite o número da conta: ");
+            try {
+                valorRetorno = scanner.nextInt();
+                return valorRetorno;
+            }
+            catch (Exception ex) {
+                System.out.println("Dado informado inválido, tente novamente.");
+                scanner.nextLine();
+            }
+        } while (true);
+    }
+
+    private static String lerString(Scanner scanner, String mensagem) {
+        String valorRetorno;
+        do {
+            System.out.print(mensagem);
+            try {
+                valorRetorno = scanner.nextLine();
+                if (valorRetorno.trim().isEmpty()){
+                    System.out.println("Não pode ser vazio!");
+                    continue;
+                }
+                return valorRetorno;
+            }
+            catch (Exception ex) {
+                System.out.println("Dado informado inválido, tente novamente.");
+                scanner.nextLine();
+            }
+        } while (true);
+    }
+
+    private static double lerDouble(Scanner scanner) {
+        double valorRetorno;
+        do {
+            System.out.print("Digite o seu saldo: ");
+            try {
+                valorRetorno = scanner.nextDouble();
+                return valorRetorno;
+            }
+            catch (Exception ex) {
+                System.out.println("Dado informado inválido, tente novamente.");
+                scanner.nextLine();
+            }
+        } while (true);
+    }
 }
+
